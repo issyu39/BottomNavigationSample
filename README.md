235fdfab0ab01652317328e446d35ff01c5061ef
Yuichi Ishibashi <redolphiner@gmail.com>
2020-03-15 18:26:32 +0900

    README.mdを追加

---
+++ README.md
@@ -0,0 +1,6 @@
+# BottomNavigationSample
+BottomNavigationView applied WindowInsets
+
+## ScreenShot
+
+<img src="https://user-images.githubusercontent.com/16067422/76698764-0f1a8c80-66ea-11ea-91ca-5daa13eb1533.png" width="300"/>
--- app/src/main/java/com/example/bottomnavigationsample/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationsample/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.bottomnavigationsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.bottom_navigation
@@ -16,9 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.decorView.systemUiVisibility =
-            SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or SYSTEM_UI_FLAG_LAYOUT_STABLE
 
         val navController = findNavController(R.id.nav_host_fragment)
         setupWithNavController(bottom_navigation, navController)
